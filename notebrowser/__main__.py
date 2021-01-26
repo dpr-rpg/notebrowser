5315a53d32dd14148d9b5392e02d112dd4e8a4ac
dpr-rpg <daniel.paul.rice+rpg@gmail.com>
2021-01-26 12:58:43 -0500

    Create SiteData object.

--- notebrowser/__main__.py
+++ notebrowser/__main__.py
@@ -4,21 +4,12 @@ from pathlib import Path
 from sys import argv
 
 from notebrowser.campaigndata import load_campaign_data
-from notebrowser.rendering import create_record_pages, create_record_toc
+from notebrowser.sitedata import create_site_data
 
 base_dir = Path(f"{os.getcwd()}") / Path(argv[1])
 assert base_dir.exists()
 assert base_dir.is_dir()
 
-site_dir = base_dir / "site"
-record_page_dir = site_dir / "records"
-record_toc_path = site_dir / "record_toc.html"
-
-site_dir.mkdir(parents=False, exist_ok=True)
-record_page_dir.mkdir(parents=False, exist_ok=True)
-assert record_page_dir.exists()
-assert record_page_dir.is_dir()
-
+site_data = create_site_data(base_dir)
 campaign_data = load_campaign_data(base_dir)
-create_record_pages(campaign_data.records, record_page_dir)
-create_record_toc(campaign_data.records, record_toc_path, record_page_dir)
+site_data.make_directories()
---
+++ notebrowser/sitedata.py
@@ -0,0 +1,43 @@
+"""Classes and functions for building the site."""
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict
+
+from notebrowser.records import RecordType
+
+
+@dataclass(frozen=True)
+class SiteData:
+    """Container for site data."""
+
+    site_dir: Path
+    record_page_dir: Path
+    session_page_dir: Path
+    note_page_dir: Path
+
+    home_page: Path
+    session_log_page: Path
+    note_toc_page: Path
+    record_toc_pages: Dict[RecordType, Path]
+
+    def make_directories(self) -> None:
+        """Make create site directories on the filesystem."""
+        self.site_dir.mkdir(parents=False, exist_ok=True)
+        self.record_page_dir.mkdir(parents=False, exist_ok=True)
+        self.session_page_dir.mkdir(parents=False, exist_ok=True)
+        self.note_page_dir.mkdir(parents=False, exist_ok=True)
+
+
+def create_site_data(base_dir: Path) -> SiteData:
+    """Create site SiteData."""
+    site_dir = base_dir / "site"
+    return SiteData(
+        site_dir,
+        site_dir / "records",
+        site_dir / "sessions",
+        site_dir / "notes",
+        site_dir / "home.html",
+        site_dir / "session_log.html",
+        site_dir / "note_toc.html",
+        {rt: site_dir / f"{rt.value}s_toc.html" for rt in RecordType},
+    )
