8a373881cfda01b0c309772ca3b2d0304f54cf33
dpr-rpg <daniel.paul.rice+rpg@gmail.com>
2021-02-03 07:55:47 -0500

    Add css stylesheet.

--- notebrowser/building.py
+++ notebrowser/building.py
@@ -12,6 +12,7 @@ def make(base_dir: Path) -> None:
     site_data = create_site_data(base_dir)
     pages = render_pages(site_data)
     make_directories(site_data)
+    write_stylesheet(site_data)
     write_pages(pages)
 
 
@@ -19,7 +20,14 @@ def make_directories(site_data: SiteData) -> None:
     """(Re)make site directories on the filesystem."""
     shutil.rmtree(site_data.site_dir)
     site_data.site_dir.mkdir(parents=False, exist_ok=True)
+    site_data.asset_dir.mkdir(parents=False, exist_ok=True)
     site_data.record_page_dir.mkdir(parents=False, exist_ok=True)
+
+
+def write_stylesheet(site_data: SiteData) -> None:
+    """Write stylesheet file."""
+    with open(site_data.stylesheet_path, "w") as cssfile:
+        cssfile.write(site_data.stylesheet_content)
 
 
 def render_pages(site_data: SiteData) -> dict[Path, str]:
--- notebrowser/sitedata.py
+++ notebrowser/sitedata.py
@@ -1,7 +1,9 @@
 """Class for storing the directory structure of the site."""
+import importlib.resources
 from dataclasses import dataclass
 from pathlib import Path
 
+import notebrowser.templates
 from notebrowser.loading import load_records
 from notebrowser.records import Record
 from notebrowser.uri import URI, Library
@@ -12,11 +14,15 @@ class SiteData:
     """Container for site data."""
 
     site_dir: Path
+    asset_dir: Path
     homepage: Path
 
     records: Library[Record]
     record_page_dir: Path
     record_pages: Library[Path]
+
+    stylesheet_path: Path
+    stylesheet_content: str
 
 
 def record_page_url(record_page_dir: Path, uri: URI) -> Path:
@@ -27,10 +33,18 @@ def record_page_url(record_page_dir: Path, uri: URI) -> Path:
 def create_site_data(base_dir: Path) -> SiteData:
     """Create site SiteData."""
     site_dir = base_dir / "site"
+    asset_dir = site_dir / "assets"
     record_source_dir = base_dir / "records"
     records = load_records(record_source_dir)
     record_page_dir = site_dir / "records"
     record_pages = {uri: record_page_url(record_page_dir, uri) for uri in records}
     return SiteData(
-        site_dir, site_dir / "home.html", records, record_page_dir, record_pages
+        site_dir,
+        asset_dir,
+        site_dir / "home.html",
+        records,
+        record_page_dir,
+        record_pages,
+        asset_dir / "style.css",
+        importlib.resources.read_text(notebrowser.templates, "style.css"),
     )
