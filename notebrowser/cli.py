17bdd8598307b65c008b1cd21ee85f6489e05f23
dpr-rpg <daniel.paul.rice+rpg@gmail.com>
2021-02-05 11:13:07 -0500

    Implement server.

--- notebrowser/cli.py
+++ notebrowser/cli.py
@@ -1,4 +1,7 @@
 """Generate static site from GM notes."""
+import http.server
+import os
+import socketserver
 from pathlib import Path
 
 import click
@@ -24,7 +27,7 @@ def cli(ctx, directory: str):
 @click.pass_obj
 def init(base_dir):
     """Initialize project directory and deploy default assets."""
-    click.echo(f"initializing project at {base_dir.absolute()}")
+    click.echo(f"Initializing project at {base_dir.absolute()}")
     building.initialize_project_directory(base_dir)
     building.deploy_default_assets(base_dir)
 
@@ -34,7 +37,7 @@ def init(base_dir):
 def make(base_dir):
     """Make site."""
     site_data = building.make_site(base_dir)
-    click.echo(f"making site at {site_data.site_dir.absolute()}")
+    click.echo(f"Made site at {site_data.site_dir.absolute()}")
 
 
 @cli.command(help="Make site pages and start local http server.")
@@ -42,5 +45,18 @@ def make(base_dir):
 def serve(base_dir):
     """Serve site."""
     site_data = building.make_site(base_dir)
-    click.echo(f"making site at {site_data.site_dir.absolute()}")
-    click.echo(f"TODO: serving site at {site_data.site_dir.absolute()}")
+    click.echo(f"Made site at {site_data.site_dir.absolute()}")
+    _serve_site(site_data.site_dir, site_data.homepage)
+
+
+def _serve_site(site_dir: Path, homepage: Path):
+    os.chdir(site_dir)
+    HOST = "localhost"
+    PORT = 8000
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
+        click.echo(f"Serving site at port {PORT}")
+        click.echo(
+            f"To browse, open a web browser and navigate to {HOST}:{PORT}{homepage}"
+        )
+        httpd.serve_forever()
